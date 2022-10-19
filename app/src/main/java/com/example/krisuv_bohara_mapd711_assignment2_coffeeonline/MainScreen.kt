e276e0373565e0c09b48972901c4e3a68b19cb59
Krisuv16 <krisuv16@gmail.com>
2022-10-19 18:15:09 -0400

    Added Validation to main screen

--- app/src/main/java/com/example/krisuv_bohara_mapd711_assignment2_coffeeonline/MainScreen.kt
+++ app/src/main/java/com/example/krisuv_bohara_mapd711_assignment2_coffeeonline/MainScreen.kt
@@ -1,14 +1,20 @@
 package com.example.krisuv_bohara_mapd711_assignment2_coffeeonline
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_screen)
-        setTitle(R.string.store_name)
+        val btnClick = findViewById<View>(R.id.outlinedButton) as Button
+        btnClick.setOnClickListener {
+            val intent = Intent(this, OrderScreen:: class.java)
+            startActivity(intent)
+        }
     }
-    
 }
